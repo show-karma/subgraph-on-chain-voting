aca6c1dc53cc3224901e04ed6d93afdef1e35c4a
Arthur <32548365+Arthh@users.noreply.github.com>
2022-04-27 09:43:43 -0300

    features: vita integration

--- generated/UniswapGovernorAlpha/UniswapGovernorAlpha.ts
+++ generated/UniswapGovernorAlpha/UniswapGovernorAlpha.ts
@@ -339,7 +339,9 @@ export class UniswapGovernorAlpha extends ethereum.SmartContract {
       ]
     );
 
-    return result[0].toTuple() as UniswapGovernorAlpha__getReceiptResultValue0Struct;
+    return changetype<UniswapGovernorAlpha__getReceiptResultValue0Struct>(
+      result[0].toTuple()
+    );
   }
 
   try_getReceipt(
@@ -359,7 +361,9 @@ export class UniswapGovernorAlpha extends ethereum.SmartContract {
     }
     let value = result.value;
     return ethereum.CallResult.fromValue(
-      value[0].toTuple() as UniswapGovernorAlpha__getReceiptResultValue0Struct
+      changetype<UniswapGovernorAlpha__getReceiptResultValue0Struct>(
+        value[0].toTuple()
+      )
     );
   }
 
---
+++ src/aave_mapping.ts
@@ -0,0 +1,63 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  AaveGovernor,
+  ProposalCreated,
+  StatusChangeToExecuted,
+  StatusChangeToVoting,
+  VoteEmitted,
+} from "../generated/AaveGovernor/AaveGovernor";
+import { User, Vote, Proposal, Organization } from "../generated/schema";
+import { getProposalId } from "./proposals";
+const daoName = "fuse.eth";
+
+export function handleProposalCreated(event: ProposalCreated): void {
+  let proposal = new Proposal(getProposalId(daoName, event.params.proposalId));
+  proposal.status = "Active";
+  proposal.timestamp = event.block.timestamp;
+  proposal.description = event.params.proposalType.toHexString();
+  proposal.proposer = event.params.proposalExecutor.toHexString();
+  let org = new Organization(daoName);
+  org.save();
+  proposal.organization = org.id;
+  proposal.save();
+}
+
+export function handleProposalExecuted(event: StatusChangeToExecuted): void {
+  let proposal = Proposal.load(getProposalId(daoName, event.params.proposalId));
+  if (proposal != null) {
+    proposal.status = "Executed";
+    proposal.timestamp = event.block.timestamp;
+    proposal.save();
+  }
+}
+
+export function handleProposalQueued(event: StatusChangeToVoting): void {
+  let proposal = Proposal.load(getProposalId(daoName, event.params.proposalId));
+  if (proposal != null) {
+    proposal.status = "Queued";
+    proposal.timestamp = event.block.timestamp;
+    proposal.save();
+  }
+}
+
+export function handleVoteCast(event: VoteEmitted): void {
+  let vote = new Vote(
+    event.params.voter.toHexString() + event.params.proposalId.toHexString()
+  );
+  let proposal = Proposal.load(getProposalId(daoName, event.params.proposalId));
+  let user = User.load(event.params.voter.toHexString());
+  if (user == null) {
+    user = new User(event.params.voter.toHexString());
+  }
+  let org = new Organization(daoName);
+  user.save();
+  if (proposal != null) {
+    vote.proposal = proposal.id;
+  }
+  vote.user = user.id;
+  vote.support = event.params.vote == new BigInt(1) ? 1 : 0;
+  vote.weight = event.params.vote;
+  vote.timestamp = event.block.timestamp;
+  vote.organization = org.id;
+  vote.save();
+}
